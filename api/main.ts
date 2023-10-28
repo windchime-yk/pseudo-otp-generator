9c1b22a35e0659fd01ddf7964501379a8957693b
WhyK <windchimeyk@gmail.com>
2023-10-28 11:23:37 +0900

    feat(api): implement pseudo OTP generator API

---
+++ api/main.ts
@@ -0,0 +1,49 @@
+import { Hono, HTTPException } from "hono";
+import { Status } from "std/http/http_status.ts";
+import { CONSTANTS, getOtpInfo, isAuth } from "~/api/core.ts";
+
+const app = new Hono();
+
+app.all("*", (ctx, next) => {
+  if (isAuth(ctx.req.raw.headers.get(CONSTANTS.ENV_KEY))) {
+    throw new HTTPException(Status.Unauthorized, {
+      message: "認証できるAPIキーを用意してください",
+    });
+  }
+  return next();
+});
+
+app.get("/otp/:service{.+}", async (ctx) => {
+  const service = ctx.req.param("service");
+  const { expired } = ctx.req.query();
+  const kv = await Deno.openKv();
+  const otpInfo = await getOtpInfo(
+    kv,
+    service,
+    expired ? Number(expired) : 86_400,
+  );
+
+  return ctx.json(otpInfo);
+});
+
+app.all("*", (ctx) => {
+  return ctx.json({
+    status: Status.NotFound,
+    message: "お探しのものは存在しません",
+  }, Status.NotFound);
+});
+
+app.onError((err, ctx) => {
+  if (err instanceof HTTPException) {
+    return ctx.json({
+      status: err.status,
+      message: err.message,
+    }, err.status);
+  }
+  return ctx.json({
+    status: Status.ServiceUnavailable,
+    message: "何らかのエラーが発生しているようです",
+  }, Status.ServiceUnavailable);
+});
+
+Deno.serve(app.fetch);
